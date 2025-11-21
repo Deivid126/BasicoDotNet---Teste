d821765cd57f95698132aed46b9df3a123bf9627
Deivid <dgoncalves484@gmail.com>
2025-11-21 09:44:22 -0300

    add validator

---
+++ 2-Application/Bernhoeft.GRT.Teste.Application/Requests/Queries/v1/Validations/GetAvisoRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Bernhoeft.GRT.Teste.Application.Requests.Queries.v1.Validations
+{
+    public class GetAvisoRequestValidator : AbstractValidator<GetAvisoRequest>
+    {
+        public GetAvisoRequestValidator()
+        {
+            RuleFor(x => x.Id)
+                .GreaterThan(0)
+                .WithMessage("O Id deve ser maior que zero.");
+        }
+    }
+}
